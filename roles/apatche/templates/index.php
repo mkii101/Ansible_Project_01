ab1021dcc4e5c9398525c806fa5976408c350501
mkkii101 <mohamed.khaledx8@gmail.com>
2025-04-12 11:53:56 +0200

    Adding Project files

---
+++ roles/apatche/templates/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>LAMP Stack Test</title>
+</head>
+<body>
+    <h1>LAMP Stack Deployment Successful!</h1>
+    <p>This page is made by Mohamed Khaled</p>
+    <p>ROUTE ACADEMY</p>
+</body>
+</html>
